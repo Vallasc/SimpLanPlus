d3a44a954392254e8b2031d7fbac8a0f73f49dcb
simone <fabbrus@gmail.com>
2021-04-19 16:02:51 +0200

    aggiunta grammatica SimpLanPlus

--- simplan-compiler/src/simplan_compiler/HelloRunner.java
+++ simplan-compiler/src/simplan_compiler/HelloRunner.java
@@ -1,4 +1,5 @@
 package simplan_compiler;
+import parser.*;
 
 import org.antlr.v4.runtime.*;
 import org.antlr.v4.runtime.tree.*;
@@ -11,12 +12,20 @@ public class HelloRunner
 		//ANTLRInputStream input = new ANTLRInputStream( System.in);
 		ANTLRInputStream input = new ANTLRInputStream("hello there");
 
-		SimpLanLexer lexer = new SimpLanLexer(input);
+		SimpLanPlusLexer lexer = new SimpLanPlusLexer(input);
+		
+		
 
 		CommonTokenStream tokens = new CommonTokenStream(lexer);
 
-		SimpLanParser parser = new SimpLanParser(tokens);
-		ParseTree tree = parser.prog(); // begin parsing at rule 'prog'
+		SimpLanPlusParser parser = new SimpLanPlusParser(tokens);
+		ParseTree tree = parser.block(); // begin parsing at rule 'prog'
 		System.out.println(tree.toStringTree(parser)); // print LISP-style tree
+		
+		//fase 1: stampare errori lessicali - scoprire come si può fare con antlr in automatico
+		//fase 2: stampare errori semantici - fare a mano nodi dell'AST oppure scoprire come farlo in automatico
+		//fase 3: controllo dei tipi ???
+		
+		
 	}
 }
