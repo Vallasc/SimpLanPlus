788e9f388aa7e5dd5dcaeea22e964203a79736cc
Vallasc <giacomovallorani.97@gmail.com>
2021-04-20 18:46:56 +0200

    Add error listener

---
+++ src/listeners/SyntaxErrorListener.java
@@ -0,0 +1,58 @@
+package listeners;
+
+import java.io.IOException;
+import java.util.BitSet;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import org.antlr.v4.runtime.ANTLRErrorListener;
+import org.antlr.v4.runtime.Parser;
+import org.antlr.v4.runtime.RecognitionException;
+import org.antlr.v4.runtime.Recognizer;
+import org.antlr.v4.runtime.atn.ATNConfigSet;
+import org.antlr.v4.runtime.dfa.DFA;
+
+
+/**
+ * A syntax error listener.
+ */
+public class SyntaxErrorListener implements ANTLRErrorListener {
+
+	private final Logger LOGGER;
+	private boolean errors = false;
+
+	/**
+	 * @param logger
+	 */
+	public SyntaxErrorListener(Logger logger) {
+		this.LOGGER = logger;
+	}
+
+	@Override
+	public void syntaxError(Recognizer<?, ?> recognizer, Object offendingSymbol, int line, int charPositionInLine,
+			String msg, RecognitionException e) {
+		if (LOGGER != null)
+			LOGGER.log(Level.SEVERE, "SYNTAX ERROR: ["+ line +":"+ charPositionInLine +"] - "+ msg);
+		errors = true;
+	}
+
+	@Override
+	public void reportAmbiguity(Parser recognizer, DFA dfa, int startIndex, int stopIndex, boolean exact,
+			BitSet ambigAlts, ATNConfigSet configs) {
+	}
+
+	@Override
+	public void reportAttemptingFullContext(Parser recognizer, DFA dfa, int startIndex, int stopIndex,
+			BitSet conflictingAlts, ATNConfigSet configs) {
+	}
+
+	@Override
+	public void reportContextSensitivity(Parser recognizer, DFA dfa, int startIndex, int stopIndex, int prediction,
+			ATNConfigSet configs) {
+	}
+
+	public boolean errorsDetected() {
+		return errors;
+	}
+
+}
--- src/main/Main.java
+++ src/main/Main.java
@@ -2,15 +2,20 @@ package main;
 
 import ast.SimpLanPlusVisitorImpl;
 import parser.*;
+import listeners.SyntaxErrorListener;
 
 import org.antlr.v4.runtime.*;
 import org.antlr.v4.runtime.tree.*;
 
 import java.io.FileInputStream;
+import java.util.logging.Logger;
 
 public class Main
 {
-	public static void main( String[] args) throws Exception 
+	private final static Logger LOGGER = Logger.getLogger(Main.class.getName());
+	private static SyntaxErrorListener parserErrorsListener;
+
+	public static void main( String[] args) throws Exception
 	{
 		String fileName = "prova.slp";
 
@@ -20,7 +25,19 @@ public class Main
 		SimpLanPlusLexer lexer = new SimpLanPlusLexer(input);
 		CommonTokenStream tokens = new CommonTokenStream(lexer);
 		SimpLanPlusParser parser = new SimpLanPlusParser(tokens);
-		ParseTree tree = parser.block(); // begin parsing at rule 'prog'
+
+		parser.removeErrorListener(ConsoleErrorListener.INSTANCE);
+		parserErrorsListener = new SyntaxErrorListener(LOGGER);
+		parser.addErrorListener(parserErrorsListener);
+
+		// Tell the parser to build the AST
+		//parser.setBuildParseTree(true);
+
+		ParseTree tree = parser.block(); // begin parsing at rule 'block'
+
+		if(parserErrorsListener.errorsDetected())
+			System.exit(-1);
+
 		System.out.println(tree.toStringTree(parser)); // print LISP-style tree
 		SimpLanPlusVisitorImpl visitor = new SimpLanPlusVisitorImpl();
 		Object ast = visitor.visit(tree); // Generazione AST
@@ -28,7 +45,6 @@ public class Main
 		//fase 1: stampare errori lessicali - scoprire come si può fare con antlr in automatico
 		//fase 2: stampare errori semantici - fare a mano nodi dell'AST oppure scoprire come farlo in automatico
 		//fase 3: controllo dei tipi ???
-		
-		
+
 	}
 }
