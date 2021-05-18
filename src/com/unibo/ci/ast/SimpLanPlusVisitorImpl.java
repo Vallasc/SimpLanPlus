8a791c8edcb3a045c90a9f5386d1ab97fdcbe8ea
Vallasc <giacomovallorani.97@gmail.com>
2021-05-18 20:15:01 +0200

    Add declarations

--- src/com/unibo/ci/ast/SimpLanPlusVisitorImpl.java
+++ src/com/unibo/ci/ast/SimpLanPlusVisitorImpl.java
@@ -1,6 +1,18 @@
 package com.unibo.ci.ast;
 
+import java.util.ArrayList;
+
+import com.unibo.ci.ast.dec.Arg;
+import com.unibo.ci.ast.dec.DecFun;
+import com.unibo.ci.ast.dec.DecVar;
+import com.unibo.ci.ast.exp.Exp;
+import com.unibo.ci.ast.types.Type;
+import com.unibo.ci.ast.types.TypeBool;
+import com.unibo.ci.ast.types.TypeInt;
+import com.unibo.ci.ast.types.TypePointer;
+import com.unibo.ci.ast.types.TypeVoid;
 import com.unibo.ci.parser.*;
+import com.unibo.ci.parser.SimpLanPlusParser.ArgContext;
 
 public class SimpLanPlusVisitorImpl extends SimpLanPlusBaseVisitor<Node> {
 
@@ -14,16 +26,43 @@ public class SimpLanPlusVisitorImpl extends SimpLanPlusBaseVisitor<Node> {
     public Node visitDeclaration(SimpLanPlusParser.DeclarationContext ctx) { return visitChildren(ctx); }
 
     @Override 
-    public Node visitDecFun(SimpLanPlusParser.DecFunContext ctx) { return visitChildren(ctx); }
+    public DecFun visitDecFun(SimpLanPlusParser.DecFunContext ctx) {
+        ArrayList<Arg> args = new ArrayList<>();
+        for (ArgContext argC : ctx.arg())
+            args.add( (Arg) visitChildren(argC));
+        Type type = ctx.type() == null ? new TypeVoid() : (Type) visitChildren(ctx.type());
+        return new DecFun(ctx.start.getLine(), ctx.start.getCharPositionInLine(), 
+                            type, 
+                            args,
+                            (Block) visitChildren(ctx.block()));
+    }
 
     @Override 
-    public Node visitDecVar(SimpLanPlusParser.DecVarContext ctx) { return visitChildren(ctx); }
+    public DecVar visitDecVar(SimpLanPlusParser.DecVarContext ctx) {
+        return new DecVar(ctx.start.getLine(), ctx.start.getCharPositionInLine(), 
+                            ctx.ID().getText(),
+                            (Type) visitChildren(ctx.type()),
+                            (Exp) visitChildren(ctx.exp()));
+    }
 
     @Override 
-    public Node visitType(SimpLanPlusParser.TypeContext ctx) { return visitChildren(ctx); }
+    public Type visitType(SimpLanPlusParser.TypeContext ctx) { 
+        if( ctx.type() != null )
+            return new TypePointer(ctx.start.getLine(), ctx.start.getCharPositionInLine(), 
+                                    (Type) visitChildren(ctx.type()));
+        if( ctx.getText().equals("int"))
+            return new TypeInt(ctx.start.getLine(), ctx.start.getCharPositionInLine());
+        if( ctx.getText().equals("bool"))
+            return new TypeBool(ctx.start.getLine(), ctx.start.getCharPositionInLine());
+
+        // Not reachable
+        return null;
+    }
 
     @Override 
-    public Node visitArg(SimpLanPlusParser.ArgContext ctx) { return visitChildren(ctx); }
+    public Arg visitArg(SimpLanPlusParser.ArgContext ctx) {
+        return (Arg)visitChildren(ctx);
+    }
 
     @Override 
     public Node visitAssignment(SimpLanPlusParser.AssignmentContext ctx) { return visitChildren(ctx); }
@@ -47,29 +86,29 @@ public class SimpLanPlusVisitorImpl extends SimpLanPlusBaseVisitor<Node> {
     public Node visitCall(SimpLanPlusParser.CallContext ctx) { return visitChildren(ctx); }
 
     @Override 
-    public Node visitBaseExp(SimpLanPlusParser.BaseExpContext ctx) { return visitChildren(ctx); }
+    public Exp visitBaseExp(SimpLanPlusParser.BaseExpContext ctx) { return (Exp) visitChildren(ctx); }
 
     @Override 
-    public Node visitBinExp(SimpLanPlusParser.BinExpContext ctx) { return visitChildren(ctx); }
+    public Exp visitBinExp(SimpLanPlusParser.BinExpContext ctx) { return (Exp) visitChildren(ctx); }
 
     @Override 
-    public Node visitDerExp(SimpLanPlusParser.DerExpContext ctx) { return visitChildren(ctx); }
+    public Exp visitDerExp(SimpLanPlusParser.DerExpContext ctx) { return (Exp) visitChildren(ctx); }
 
     @Override 
-    public Node visitNewExp(SimpLanPlusParser.NewExpContext ctx) { return visitChildren(ctx); }
+    public Exp visitNewExp(SimpLanPlusParser.NewExpContext ctx) { return (Exp) visitChildren(ctx); }
 
     @Override 
-    public Node visitValExp(SimpLanPlusParser.ValExpContext ctx) { return visitChildren(ctx); }
+    public Exp visitValExp(SimpLanPlusParser.ValExpContext ctx) { return (Exp) visitChildren(ctx); }
 
     @Override 
-    public Node visitNegExp(SimpLanPlusParser.NegExpContext ctx) { return visitChildren(ctx); }
+    public Exp visitNegExp(SimpLanPlusParser.NegExpContext ctx) { return (Exp) visitChildren(ctx); }
 
     @Override 
-    public Node visitBoolExp(SimpLanPlusParser.BoolExpContext ctx) { return visitChildren(ctx); }
+    public Exp visitBoolExp(SimpLanPlusParser.BoolExpContext ctx) { return (Exp) visitChildren(ctx); }
 
     @Override 
-    public Node visitCallExp(SimpLanPlusParser.CallExpContext ctx) { return visitChildren(ctx); }
+    public Exp visitCallExp(SimpLanPlusParser.CallExpContext ctx) { return (Exp) visitChildren(ctx); }
 
     @Override 
-    public Node visitNotExp(SimpLanPlusParser.NotExpContext ctx) { return visitChildren(ctx); }
+    public Exp visitNotExp(SimpLanPlusParser.NotExpContext ctx) { return (Exp) visitChildren(ctx); }
 }
