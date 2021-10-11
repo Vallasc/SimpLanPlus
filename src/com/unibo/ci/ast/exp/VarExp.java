b0487d1317533d1a1c3fccfc6b5e4a5c9f39ba71
Giacomo Vallorani <giacomovallorani.97@gmail.com>
2021-10-11 23:19:41 +0200

    Initial commit

---
+++ src/com/unibo/ci/ast/exp/VarExp.java
@@ -0,0 +1,46 @@
+package com.unibo.ci.ast.exp;
+
+import java.util.ArrayList;
+
+import com.unibo.ci.ast.errors.SemanticError;
+import com.unibo.ci.ast.types.Type;
+import com.unibo.ci.util.Environment;
+import com.unibo.ci.util.STentry;
+
+public class VarExp extends Exp {
+    private final String id;
+    private STentry stEntry;
+
+    public VarExp(int row, int column, String id) {
+        super(row, column);
+        this.id = id;
+    }
+
+    @Override
+    public String toPrint(String indent) {
+        return indent + "Exp: Var\n" + 
+                indent + "\t Id: " + id + "\n";
+    }
+
+    @Override
+    public ArrayList<SemanticError> checkSemantics(Environment env) {
+		ArrayList<SemanticError> errors = new ArrayList<SemanticError>();
+		stEntry = env.lookupSTentry(id);
+		if (stEntry == null)
+			errors.add(new SemanticError(row, column, "var " + id + " does not exist"));
+		return errors;
+    }
+
+    @Override
+    public Type typeCheck() { 
+        if(stEntry == null)
+            return null;
+        return stEntry.getType();
+    }
+
+    @Override
+    public String codeGeneration() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+}
