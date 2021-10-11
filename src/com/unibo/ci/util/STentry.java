29b735bbcaae237e541b95aa3a4a11c3be3bf55c
Giacomo Vallorani <giacomovallorani.97@gmail.com>
2021-10-11 23:20:23 +0200

    Update

--- src/com/unibo/ci/util/STentry.java
+++ src/com/unibo/ci/util/STentry.java
@@ -4,22 +4,20 @@ import com.unibo.ci.ast.types.Type;
 
 public class STentry {
 
-    private int nestLevel;
-    private Type type;
-    private String id;
-    private int offset;
-    private boolean deleted;
+    private final int nestLevel;
+    private final Type type;
+    private final String id;
+    private final int offset;
+    private boolean deleted; // TODO non serve in questa tabella
 
-    public STentry(int nestLevel, int offset, Type type) {
+    public STentry(String id, Type type, int nestLevel, int offset) {
         this.nestLevel = nestLevel;
         this.offset = offset;
         this.type = type;
-        this.setDeleted(false);
+        this.deleted = false;
+        this.id = id;
     }
 
-    public void addType(Type type) {
-        this.type = type;
-    }
 
     public Type getType() {
         return type;
@@ -33,19 +31,17 @@ public class STentry {
         return nestLevel;
     }
 
-    /*
-     * public String toPrint(String indent) { return indent + "STentry: nestlev " +
-     * Integer.toString(nestLevel +"\n"+ indent + "STentry: type\n" +
-     * type.toPrint(indent + "  ") + indent + "STentry: offset " +
-     * Integer.toString(offset) + "\n"; }
-     */
-
     public String getId() {
         return id;
     }
 
-    public void setId(String id) {
-        this.id = id;
+
+    public String toPrint(String indent) { 
+        return indent + "STentry:\n" + 
+            indent + "\tNesting Level: " + nestLevel + "\n" +
+            indent + "\tId: \"" + this.id + "\"\n" +
+            type.toPrint(indent + "\t") + 
+            indent + "\tOffset: \"" + this.offset + "\"\n";
     }
 
     public boolean isDeleted() {
