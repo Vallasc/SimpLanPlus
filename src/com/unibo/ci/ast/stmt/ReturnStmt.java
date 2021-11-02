4fd392192f029e21b952cafe3b8a32473f05ec32
andiub97 <43032657+andiub97@users.noreply.github.com>
2021-11-02 14:06:27 +0100

    add return type check

--- src/com/unibo/ci/ast/stmt/ReturnStmt.java
+++ src/com/unibo/ci/ast/stmt/ReturnStmt.java
@@ -2,20 +2,24 @@ package com.unibo.ci.ast.stmt;
 
 import java.util.ArrayList;
 
-import com.unibo.ci.ast.errors.EffectError;
 import com.unibo.ci.ast.errors.SemanticError;
 import com.unibo.ci.ast.errors.TypeError;
 import com.unibo.ci.ast.exp.Exp;
 import com.unibo.ci.ast.types.Type;
 import com.unibo.ci.ast.types.TypeBool;
+import com.unibo.ci.ast.types.TypeFunction;
 import com.unibo.ci.ast.types.TypeInt;
 import com.unibo.ci.ast.types.TypeVoid;
 import com.unibo.ci.util.Environment;
+import com.unibo.ci.util.STentry;
 import com.unibo.ci.util.TypeErrorsStorage;
+
+// tipo ritorno = tipo funzione
 
 public class ReturnStmt extends Statement {
 
     private Exp exp;
+    private STentry funStEntry;
 
     public ReturnStmt(int row, int column, Exp exp) {
         super(row, column);
@@ -24,35 +28,51 @@ public class ReturnStmt extends Statement {
 
     @Override
     public String toPrint(String indent) {
-        return indent + "Return: \n" + 
-                (exp != null ? exp.toPrint(indent + "\t") : indent + "\tvoid");
+        // TODO Auto-generated method stub
+        return null;
     }
 
     @Override
     public ArrayList<SemanticError> checkSemantics(Environment env) {
-        if(exp != null)
-            return exp.checkSemantics(env);
-        else 
-            return new ArrayList<SemanticError>();
+        ArrayList<SemanticError> errors = new ArrayList<SemanticError>();
+
+        exp.checkSemantics(env);
+
+        funStEntry = env.lookupFunction();
+        /*
+         * Non concesso nel main { return; }
+         */
+        if (funStEntry == null) {
+            errors.add(new SemanticError(super.column, super.row, "No function to return."));
+        }
+        return errors;
     }
 
     @Override
     public Type typeCheck() {
-        if (exp == null) 
-            return new TypeVoid();
+        if (funStEntry == null) {
+            return null;
+        }
 
-        Type returnType = exp.typeCheck();
+        Type returnType = ((TypeFunction) funStEntry.getType());
 
-        if(returnType == null)
+        if (exp == null && !returnType.equals(new TypeVoid())) {
+            TypeErrorsStorage.add(
+                    new TypeError(super.column, super.row, "Return type must be [" + returnType.getTypeName() + "]."));
+        } else if (!returnType.equals(exp.typeCheck())) {
+            TypeErrorsStorage.add(
+                    new TypeError(super.column, super.row, "Return type must be [" + returnType.getTypeName() + "]."));
+        }
+        // controllare che il tipo sia int o bool o void
+        if (exp != null && (exp.typeCheck() instanceof TypeInt || exp.typeCheck() instanceof TypeVoid
+                || exp.typeCheck() instanceof TypeBool)) {
+            return returnType;
+        } else {
+            TypeErrorsStorage.add(new TypeError(super.row, super.column, "incorrect return type"));
             return null;
 
-        // controllare che il tipo sia int o bool o void
-        if(returnType instanceof TypeInt || returnType instanceof TypeBool)
-            return returnType;
+        }
 
-        TypeErrorsStorage.add( new TypeError(this.exp.getRow(), this.exp.getColumn(), 
-                                    "Incompatible return type (" + returnType + ")"));
-        return null;
     }
 
     @Override
@@ -60,11 +80,5 @@ public class ReturnStmt extends Statement {
         // TODO Auto-generated method stub
         return null;
     }
-
-	@Override
-	public ArrayList<EffectError> AnalyzeEffect(Environment env) {
-		// TODO Auto-generated method stub
-		return null;
-	}
 
 }
