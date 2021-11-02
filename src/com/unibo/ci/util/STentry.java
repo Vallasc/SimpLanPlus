5ddc48f83858181a6d30acb78c9bf4d061059406
andiub97 <43032657+andiub97@users.noreply.github.com>
2021-11-02 14:27:50 +0100

    remove deleted attribute

--- src/com/unibo/ci/util/STentry.java
+++ src/com/unibo/ci/util/STentry.java
@@ -8,16 +8,13 @@ public class STentry {
     private final Type type;
     private final String id;
     private final int offset;
-    private boolean deleted; // TODO non serve in questa tabella
 
     public STentry(String id, Type type, int nestLevel, int offset) {
         this.nestLevel = nestLevel;
         this.offset = offset;
         this.type = type;
-        this.deleted = false;
         this.id = id;
     }
-
 
     public Type getType() {
         return type;
@@ -35,21 +32,9 @@ public class STentry {
         return id;
     }
 
-
-    public String toPrint(String indent) { 
-        return indent + "STentry:\n" + 
-            indent + "\tNesting Level: " + nestLevel + "\n" +
-            indent + "\tId: \"" + this.id + "\"\n" +
-            type.toPrint(indent + "\t") + 
-            indent + "\tOffset: \"" + this.offset + "\"\n";
-    }
-
-    public boolean isDeleted() {
-        return deleted;
-    }
-
-    public void setDeleted(boolean deleted) {
-        this.deleted = deleted;
+    public String toPrint(String indent) {
+        return indent + "STentry:\n" + indent + "\tNesting Level: " + nestLevel + "\n" + indent + "\tId: \"" + this.id
+                + "\"\n" + type.toPrint(indent + "\t") + indent + "\tOffset: \"" + this.offset + "\"\n";
     }
 
 }
