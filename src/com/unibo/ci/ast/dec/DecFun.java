a5a72ed14b5c5597218bad8008a81d3dcdee2769
Giacomo Vallorani <giacomovallorani.97@gmail.com>
2021-11-07 12:51:59 +0100

    Code refactor

--- src/com/unibo/ci/ast/dec/DecFun.java
+++ src/com/unibo/ci/ast/dec/DecFun.java
@@ -3,24 +3,26 @@ package com.unibo.ci.ast.dec;
 import java.util.ArrayList;
 import java.util.List;
 
-import com.unibo.ci.ast.Node;
-import com.unibo.ci.ast.errors.EffectError;
 import com.unibo.ci.ast.errors.SemanticError;
-import com.unibo.ci.ast.stmt.block.Block;
 import com.unibo.ci.ast.types.Type;
+import com.unibo.ci.ast.types.TypeVoid;
 import com.unibo.ci.util.Environment;
 import com.unibo.ci.util.Environment.DuplicateSTEntryException;
 import com.unibo.ci.ast.stmt.block.BlockBase;
 import com.unibo.ci.ast.types.TypeFunction;
+import com.unibo.ci.util.TypeErrorsStorage;
+import com.unibo.ci.ast.errors.TypeError;
 
 public class DecFun extends Dec {
     private final String id;
+    private final Type type;
     private final List<Arg> args;
     private final BlockBase block;
 
     public DecFun(int row, int column, Type type, String id, List<Arg> args, BlockBase block) {
         super(row, column, type, id);
         this.id = id;
+        this.type = type;
         this.args = args;
         this.block = block;
     }
@@ -30,11 +32,12 @@ public class DecFun extends Dec {
         return indent + "Declaration: Function\n" + 
                 indent + "\tId: \"" + this.id + "\"\n" + 
                 type.toPrint(indent + "\t") +
-                printArgs(indent + "\t");
+                printArgs(indent + "\t") + 
+                block.toPrint(indent + "\t");
     }
 
     private String printArgs(String indent) {
-        StringBuilder sb = new StringBuilder("Args: ");
+        StringBuilder sb = new StringBuilder(indent + "Args: \n");
         this.args.forEach((arg) -> {
             sb.append(arg.toPrint(indent + "\t"));
         });
@@ -47,7 +50,8 @@ public class DecFun extends Dec {
         ArrayList<SemanticError> semanticErrors = new ArrayList<SemanticError>();
         try {
             // Aggiungi tipo funzione
-            env.addDeclaration(id, type);
+            env.addDeclaration(id, new TypeFunction(row, column, id, args.size(), type, args) );
+            //TODO quanta memoria occupa la decfun? solo il numero deli argomenti? (args.size())
 
             // Nota: type dovrebbe essere T_1, ..., T_n -> T
 
@@ -56,20 +60,7 @@ public class DecFun extends Dec {
             semanticErrors.add(error);
             // return semanticErrors;
         }
-
-        env.newScope();
-
-        for (Arg arg : args) {
-            try {
-                env.addDeclaration(arg.getId(), arg.typeCheck());
-            } catch (DuplicateSTEntryException e) {
-                semanticErrors.add(new SemanticError(row, column,
-                        "Function " + id + ": repeated paramateres in function definition"));
-            }
-        }
-
-        semanticErrors.addAll(block.checkSemantics(env));
-        env.exitScope();
+        semanticErrors.addAll(block.checkSemanticsInjectArgs(env, args));
         return semanticErrors;
     }
     
@@ -79,8 +70,17 @@ public class DecFun extends Dec {
             if (arg.typeCheck() == null)
                 return null;
         }
+        
+        Type blockType = this.block.typeCheck() ;
+        //System.out.println("DEBUG: il tipo del blocco nella funzione " + id + " è " + blockType);
+        //System.out.println("DEBUG: il tipo della funzione " + id + " è " + this.type);
+        if ( (blockType == null && !(this.type instanceof TypeVoid)) || (blockType != null && !this.type.equals( blockType))){
+            //Errore! Tipo del blocco e tipo di ritorno della funzione incompatibili
+            TypeErrorsStorage.add( new TypeError(super.row, super.column, 
+                "Function [" + this.id + "] must return with type [" + type.getTypeName() +"]" ));
+        }
 
-        return block.typeCheck() == null ? null : new TypeFunction(row, column, id, args.size(), type, args); 
+        return new TypeFunction(row, column, id, args.size(), type, args); 
         //TODO quanta memoria occupa la decfun? solo il numero deli argomenti? (args.size())
     }
 
