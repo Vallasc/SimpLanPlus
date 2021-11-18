ff2589e218a3788f4e807f4c2e1d64cb11a52ac8
andiub97 <43032657+andiub97@users.noreply.github.com>
2021-11-18 17:28:22 +0100

    implement isFunction

--- src/com/unibo/ci/util/EEntry.java
+++ src/com/unibo/ci/util/EEntry.java
@@ -1,27 +1,37 @@
 package com.unibo.ci.util;
 
+import java.util.List;
+
 import com.unibo.ci.util.EffectHelper.ETypes;
 
 public class EEntry extends Entry<ETypes> {
-   
-	ETypes etype;
 
-	public EEntry(String id, ETypes type, int nestLevel, int offset) {
-		super(id, type, nestLevel, offset);
+  ETypes etype;
+
+  private List<EEntry> funEffects;
+
+  public EEntry(String id, ETypes type, int nestLevel, int offset) {
+    super(id, type, nestLevel, offset);
     this.etype = type;
-    }
-	
-    public String toPrint(String indent) {
-    	return indent + "EEentry:\n" + indent + "\tNesting Level: " + nestLevel + "\n" + indent + "\tId: \"" + this.id
-                + "\"\n" + indent + "Effect: " + type + "\n" + indent + "\tOffset: \"" + this.offset + "\"\n";
-    };
+  }
 
-    public ETypes getEtype(){
-      return super.type;
-    }
+  public String toPrint(String indent) {
+    return indent + "EEentry:\n" + indent + "\tNesting Level: " + nestLevel + "\n" + indent + "\tId: \"" + this.id
+        + "\"\n" + indent + "Effect: " + type + "\n" + indent + "\tOffset: \"" + this.offset + "\"\n";
+  };
 
-    public void updateEffectType(ETypes type) {
-    	this.type = type;
-    }
-    
+  public ETypes getEtype() {
+    return super.type;
+  }
+
+  public void updateEffectType(ETypes type) {
+    this.type = type;
+  }
+
+  @Override
+  public boolean isNotFunction() {
+
+    return funEffects != null;
+  }
+
 }
