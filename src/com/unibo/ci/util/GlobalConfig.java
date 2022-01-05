388538b303c3d7f41dd5209a1cf057eca5de2ab4
Vallasc <giacomovallorani.97@gmail.com>
2022-01-05 18:16:18 +0100

    Add new pars

--- src/com/unibo/ci/util/GlobalConfig.java
+++ src/com/unibo/ci/util/GlobalConfig.java
@@ -1,6 +1,9 @@
 package com.unibo.ci.util;
 
 public class GlobalConfig {
-    public static final boolean PRINT_COMMENTS = true;
-    public static final boolean PRINT_AST = false;
+    public static boolean PRINT_COMMENTS = false;
+    public static boolean SHOW_MEM = false;
+    public static boolean PRINT_AST = false;
+    public static String OUT_FILENAME = "out";
+    public static String INPUT_FILENAME = "";
 }
