ee6d36c9add26b9d3e0f64776977e9fff11a4f83
Vallasc <giacomovallorani.97@gmail.com>
2022-01-05 18:16:41 +0100

    Use LOGGER

--- src/com/unibo/ci/util/WarningsStorage.java
+++ src/com/unibo/ci/util/WarningsStorage.java
@@ -1,11 +1,13 @@
 package com.unibo.ci.util;
 
 import java.util.LinkedList;
+import java.util.logging.Logger;
 
 import com.unibo.ci.ast.errors.Error;
 import com.unibo.ci.ast.errors.Warning;
 
 public class WarningsStorage {
+    private final static Logger LOGGER = Logger.getLogger(WarningsStorage.class.getCanonicalName());
     private final static LinkedList<Warning> warningList = new LinkedList<>();
 
     public static void add(Warning warning) {
@@ -13,7 +15,7 @@ public class WarningsStorage {
     }
 
     public static void printAll() {
-        warningList.forEach(e -> System.out.print("⚠️ Warning in [" + e.row + ", " + e.col + "]: " + e.desc));
+        warningList.forEach(e -> LOGGER.warning("⚠️ Warning in [" + e.row + ", " + e.col + "]: " + e.desc));
     }
 
     public static LinkedList<Warning> getWarningList() {
