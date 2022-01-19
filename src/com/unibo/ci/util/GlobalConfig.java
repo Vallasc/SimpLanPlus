4fc88e810ab0add033c540c9c2881549c2cca7d9
Giacomo Vallorani <giacomovallorani.97@gmail.com>
2022-01-19 20:23:37 +0100

    Add memsize

--- src/com/unibo/ci/util/GlobalConfig.java
+++ src/com/unibo/ci/util/GlobalConfig.java
@@ -3,6 +3,7 @@ package com.unibo.ci.util;
 public class GlobalConfig {
     public static boolean PRINT_COMMENTS = false;
     public static boolean SHOW_MEM = false;
+    public static int MEM_SIZE = 500;
     public static boolean SHOW_DEBUG = false;
     public static boolean PRINT_AST = false;
     public static String OUT_FILENAME = "out";
