d8289a65d60cc15562e194f398bd83a77c18ad35
Vallasc <giacomovallrani.97@gmail.com>
2022-01-21 19:29:04 +0100

    Deleted

--- src/com/unibo/ci/ast/stmt/block/Block.java
+++
@@ -1,9 +0,0 @@
-package com.unibo.ci.ast.stmt.block;
-
-import com.unibo.ci.ast.stmt.Statement;
-
-public abstract class Block extends Statement {
-    public Block(int row, int column) {
-        super(row, column);
-    }
-}
