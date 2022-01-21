48fb24ad2ec83baae865b0d9199461ebc3f12145
Vallasc <giacomovallrani.97@gmail.com>
2022-01-21 19:44:08 +0100

    Add params

--- tests.sh
+++ tests.sh
@@ -4,4 +4,4 @@ rm -rf compiled
 mkdir compiled
 # ; windows, in *nix it is :
 javac -d compiled -cp lib/antlr-4.9.2-complete.jar:src src/com/unibo/ci/test/Test.java
-java -cp lib/antlr-4.9.2-complete.jar:compiled com.unibo.ci.test.Test
+java -cp lib/antlr-4.9.2-complete.jar:compiled com.unibo.ci.test.Test "$@"
