a34cde674380035de9e91162370ed0d00e2f0cc9
Vallasc <giacomovallrani.97@gmail.com>
2022-01-21 19:30:35 +0100

    code refactor

---
+++ src/com/unibo/ci/test/Test.java
@@ -0,0 +1,33 @@
+package com.unibo.ci.test;
+
+import com.unibo.ci.Main;
+import com.unibo.ci.util.GlobalConfig;
+
+import java.io.File;
+
+public class Test {
+
+	public static void main(String[] args) {
+		String testpath = "test/";
+
+		// uso: test.java x, dove x è il numero del test da eseguire
+		// se non viene specificato nessun numero esegui tutti i test nella testpath
+		if (args.length == 0) {
+			File testFolder = new File(testpath);
+			for (String filename : testFolder.list()) {
+				if (filename.endsWith(".slp") && filename.startsWith("test")) {
+					System.out.println("Test: " + filename);
+					System.out.println("---------------------------------------");
+					Main.main(new String[] { testpath.concat(filename) });
+					System.out.println("---------------------------------------\n");
+				}
+			}
+		} else {
+			//GlobalConfig.PRINT_COMMENTS = true;
+			//GlobalConfig.SHOW_MEM = true;
+			GlobalConfig.MEM_SIZE = 150;
+			//GlobalConfig.SHOW_DEBUG = true;
+			Main.main(new String[] { testpath + "test" + args[0] + ".slp" });
+		}
+	}
+}
